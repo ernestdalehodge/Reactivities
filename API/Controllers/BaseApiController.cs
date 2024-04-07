3c4a5a673a0c82fb857fbfa5504935786b6a1397
Ernest Hodge <ErnestHodge@Yahoo.com>
2024-04-07 18:52:04 -0500

    End of section 10

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Application.Core;
 
 namespace API.Controllers
 {
@@ -12,5 +13,14 @@ namespace API.Controllers
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null) return NotFound();
+
+            return BadRequest(result.Error);
+        }
     }
 }
